558e38a4a0246dd647b6a030945a73245bdffac5
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2023-03-07 22:37:51 +0700

    chore: init project, create project folder structure

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:advicer/theme.dart';
+import 'package:advicer/application/core/services/theme_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(builder: (context, themeService, child) {
+      return MaterialApp(
+        themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        title: 'Question List',
+        home: const Placeholder(),
+      );
+    });
+  }
+}
