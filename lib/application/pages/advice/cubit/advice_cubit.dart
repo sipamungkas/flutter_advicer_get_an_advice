bbbb86977791b7c1f8c1fe3b1eb6f5371c8e5197
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2023-03-11 13:41:33 +0700

    feat: domain layer, add dartz for functional programming, usecases, and failures

--- lib/application/pages/advice/cubit/advice_cubit.dart
+++ lib/application/pages/advice/cubit/advice_cubit.dart
@@ -1,9 +1,15 @@
 import 'package:advicer/domain/entities/advice_entity.dart';
+import 'package:advicer/domain/failures/failures.dart';
 import 'package:advicer/domain/usecases/advice_usecases.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'advice_state.dart';
+
+const generalFailureMessage = 'Oops! Something went wrong, Plase try again!';
+const serverFailureMessage = 'Server under maintenance!';
+const cacheFailureMessage = 'Oops! Cache failure, Please try again!';
 
 class AdviceCubit extends Cubit<AdviceCubitState> {
   AdviceCubit() : super(AdviceInitial());
@@ -13,8 +19,27 @@ class AdviceCubit extends Cubit<AdviceCubitState> {
   void adviceRequest() async {
     // execute business logic, example: get and advice
     emit(AdviceStateLoading());
-    final AdviceEntity advice = await adviceUsecases.getAdvice();
+    // dont need to add type it will get from the usecase
+    final failureOrAdvice = await adviceUsecases.getAdvice();
+    // fold will decide, if we got the left(failure) side or the right(advice) side from Failure Dartz
+    failureOrAdvice.fold(
+      (failure) {
+        emit(AdviceStateError(message: _mapFailureToMessage(failure)));
+      },
+      (advice) => emit(
+        AdviceStateLoaded(advice: advice.advice),
+      ),
+    );
+  }
 
-    emit(AdviceStateLoaded(advice: advice.advice));
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return generalFailureMessage;
+    }
   }
 }
--- lib/domain/usecases/advice_usecases.dart
+++ lib/domain/usecases/advice_usecases.dart
@@ -1,17 +1,26 @@
 import 'package:advicer/domain/entities/advice_entity.dart';
+import 'package:advicer/domain/failures/failures.dart';
+import 'package:dartz/dartz.dart';
 
 class AdviceUsecases {
   // usecases is just a collection of method
 
-  Future<AdviceEntity> getAdvice() async {
+  Future<Either<Failure, AdviceEntity>> getAdvice() async {
+    await Future.delayed(const Duration(seconds: 2), () {});
     // call a repository, to get data (failure or data)
     // process the data with the business logic (manipulate the data)
     // return the data
+    // call to repo when success => return data (AdviceEntity)
 
-    await Future.delayed(const Duration(seconds: 2), () {});
-    return const AdviceEntity(
+    // to return a edvice entity use Right (because advice entity in the right at either)
+    return const Right(
+      AdviceEntity(
         id: 1,
         advice:
-            'Advice from server but it\'s fake, just to test the usecases using entity');
+            'Advice from server but it\'s fake, just to test the usecases using entity',
+      ),
+    );
+    // to return an error use Left (because failure in the left at Either)
+    // return Left(ServerFailure());
   }
 }
